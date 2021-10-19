e7bdebdd38bc04216cf698ed582b1a18cc4a9016
TestadeNevill <64296826+TestadeNevill@users.noreply.github.com>
2021-10-19 17:07:42 -0400

    update documents

--- demo.js
+++ demo.js
@@ -15,11 +15,7 @@ async function main() {
     try {
         await client.connect();
 
-        await findListingsWithMinimumBedroomsBathroomsAndMostRecentReviews(client, {
-            minimumNumberOfBedrooms: 4,
-            minimumNumberOfBathrooms: 2,
-            maximumNumberOfResults: 5
-        });
+        await updateAllListingsToHavePropertyType(client)
 
 
     } catch (e) {
@@ -30,6 +26,38 @@ async function main() {
 }
 
 main().catch(console.error);
+
+async function updateAllListingsToHavePropertyType(client) {
+    const result = await client.db("sample_airbnb").collection("listingsAndReviews").updateMany({ property_type: { $exists: false } },
+        { $set: { property_type: "Unknown" } });
+
+    console.log(`${result.matchedCount} document(s) matched the query criteria`);
+    console.log(`${result.modifiedCount} document(s) was/were updated`);
+}
+
+async function upsertListingByName(client, nameOfListing, updatedListing) {
+    const result = await client.db("sample_airbnb").collection("listingsAndReviews").updateOne(
+        { name: nameOfListing }, { $set: updatedListing }, { upsert: true }
+    );
+
+    console.log(`${result.matchedCount} document(s) matched the query criteria`);
+
+    if (result.upsertedCount > 0) {
+
+        console.log(`One document was inserted with the id ${result.upsertedId}`);
+    } else {
+        console.log(`${result.modifiedCount} document(s) was/were updated`);
+    }
+}
+
+async function updateListingByName(client, nameOfListing, updatedListing) {
+    const result = await client.db("sample_airbnb").collection("listingsAndReviews").updateOne(
+        { name: nameOfListing }, { $set: updatedListing }
+    );
+
+    console.log(`${result.matchedCount} document(s) matched the query criteria`);
+    console.log(`${result.modifiedCount} document(s) was/were updated`);
+}
 
 async function findListingsWithMinimumBedroomsBathroomsAndMostRecentReviews(client, {
     minimumNumberOfBedrooms = 0,
